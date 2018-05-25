29a032e8aece4c79787192f0480e780ad9e20ea3
David Alderson <dsa027@gmail.com>
2018-05-24 19:30:18 -0700

    Initial Commit

---
+++ javascript/connect4.js
@@ -0,0 +1,348 @@
+(function() {
+  const ROWS = 6;
+  const COLS = 7;
+  const EMPTY = -1;
+  const RED = 0;
+  const YELLOW = 1;
+
+  // http://soundbible.com/1540-Computer-Error-Alert.html
+  const beep = new Audio('sounds/error.wav');
+
+  const cWidth = 490, cHeight = 420; // screen dimesions
+  const rowHeight = cHeight / ROWS;
+  const colWidth = cWidth / COLS;
+  var canvas = null;
+  var context = null;
+  var canvasRect = null; // dims where the canvas is on the page
+  var stop = true;
+  var gameOver = false;
+  var turn = RED;
+  var background;
+  var grid;
+
+  const discs = [];
+
+  var Side = {'COMPUTER': 'COMPUTER', 'PLAYER': 'PLAYER', 'NONE': 'NONE'};
+
+  class Disc {
+    constructor(x, y, color) {
+      this.radius = rowHeight / 2;
+      this.x = x;
+      this.y = y;
+      this.color = color;
+    }
+
+    render() {
+      context.beginPath();
+      context.arc(
+          this.x*colWidth + this.radius,
+          this.y*rowHeight + this.radius,
+          rowHeight/2, 0, 2 * Math.PI, false);
+      context.fillStyle = this.color;
+      context.fill();
+    }
+  }
+
+  class Grid {
+    render() {
+      context.fillStyle = 'white';
+      for (let i = 1; i < COLS; i++) {
+        context.fillRect(i * rowHeight, 0, 1, cHeight);
+      }
+      for (let i = 1; i < ROWS; i++) {
+        context.fillRect(0, i * colWidth, cWidth, 1);
+      }
+    }
+  }
+
+  //////////////////
+  class Background {
+  //////////////////
+    constructor() {
+      var image = new Image(cWidth, cHeight);
+      image.src = 'images/bg.png';
+      this.sprite = new Sprite(image, cWidth, cHeight);
+      this.justScored = Side.NONE;
+      this.gameOverFlash = new Flash(30);
+    }
+
+    showGameOver() {
+      // don't show elements unless flashing === true
+      if (!this.gameOverFlash.check()) return;
+
+      var fontSize = 76;
+      context.font = `${fontSize}px sans-serif`;
+      context.fillStyle = 'red';
+
+      var gW = context.measureText("GAME").width;
+      var oW = context.measureText("OVER").width;
+
+      var ySpace = (cHeight - fontSize*2) / 3;
+      ySpace *= .7;
+      var xSpace = (cWidth - Math.max(gW, oW)) / 2;
+
+      context.fillText("GAME", xSpace, ySpace+fontSize);
+      context.fillText("OVER", xSpace, fontSize*2 + ySpace*2);
+
+      var str = "Press any key to continue...";
+      fontSize = 32;
+      context.font = `${fontSize}px sans-serif`;
+      context.fillText(str, (cWidth - context.measureText(str).width) / 2, cHeight-5);
+    }
+
+    showClickToStart() {
+      context.fillStyle = 'palegreen';
+      var str = "Click mouse or press spacebar to serve...";
+      var fontSize = 22;
+      context.font = `${fontSize}px sans-serif`;
+      context.fillText(str, (cWidth - context.measureText(str).width) / 2, cHeight / 2 + fontSize*2);
+    }
+
+    render() {
+      // background sprite
+      context.drawImage(this.sprite.image, 0, 0, this.sprite.width, this.sprite.height);
+
+      // // player1/player2 or player/computer
+      // this.showTitles();
+      //
+      // // easy, medium, hard
+      // this.showLevelIcons();
+      //
+      // // player2 or computer
+      // this.showComputerAndPlayerIcons();
+      //
+      // // mouse/kbd
+      // this.showInputIcons();
+      //
+      // this.showScore();
+
+      if (gameOver) {
+        this.showGameOver();
+      }
+      else if (stop) {
+        // this.showClickToStart();
+      }
+    }
+  } // Background
+
+  //////////////////
+  class Flash {
+  //////////////////
+    constructor(interval) {
+      this.flashInterval = interval;
+      this.flashCount = 0;
+      this.flashing = true;
+    }
+
+    check() {
+      if (++this.flashCount >= this.flashInterval) {
+        this.flashCount = 0;
+        this.flashing = !this.flashing;
+      }
+
+      return this.flashing;
+    }
+  }
+
+  //////////////////
+  class Sprite {
+  //////////////////
+    constructor(image, width, height) {
+      this.image = image;
+      this.width = width;
+      this.height = height;
+    }
+  } // Sprite
+
+
+  //////////////////
+  document.addEventListener("DOMContentLoaded", function(event) {
+  //////////////////
+    // animate
+    function step() {
+      // at start of game/end of game? don't animate
+      if (!stop) {
+        // ball.move();
+        // cPaddle.update()
+      }
+      background.render();
+      grid.render();
+      for (let i = 0; i < discs.length; i++) {
+        discs[i].render();
+      }
+
+      requestAnimationFrame(step);
+    }
+
+    function addListeners() {
+      kbdListener();
+      mouseClickListener();
+      mouseMoveListener();
+    }
+
+    function buildCanvas() {
+      // build canvas
+      canvas = document.getElementById("canvas");
+      canvas.width = cWidth;
+      canvas.height = cHeight;
+      context = canvas.getContext("2d");
+    }
+
+    function createGameElements() {
+      background = new Background();
+      grid = new Grid();
+    }
+
+    function restartGame() {
+      gameOver = false;
+      stop = true;
+      discs.splice(0, discs.length);
+      turn = RED;
+    }
+
+    function kbdListener() {
+      // move player's paddle on keydown
+      window.addEventListener('keydown', function(event) {
+        if (gameOver) {
+          restartGame();
+          // clearWonLost();
+          // background.whichSideScored(Side.NONE);
+          return;
+        }
+        // if computer has the keyboard and playing computer, leave
+        // if (!pKbd && computer) return;
+        //
+        // var paddle = pKbd ? pPaddle : cPaddle;
+        // switch(event.keyCode) {
+        //   case 32: //spacebar
+        //     stop = false; // start play
+        //     break;
+        //   case 38: //paddle up
+        //     paddle.move(-10);
+        //     break;
+        //   case 40: //paddle down
+        //     paddle.move(10);
+        //     break;
+        // }
+      });
+    }
+
+    function mouseClickListener() {
+      // start game on click, or click on canvas elements
+      window.addEventListener('click', function(event) {
+        if (gameOver) {
+          restartGame();
+          return;
+        }
+        canvasRect = canvas.getBoundingClientRect();
+        const mouseX = event.clientX - canvasRect.left;
+
+        const x = getX(mouseX);
+        const y = getY(mouseX);
+
+        if (y < 0) {
+          beep.play();
+        }
+        else {
+          const disc = new Disc(x, y, turn === RED ? 'red' : 'yellow');
+          discs.push(disc);
+          turn = turn === RED ? YELLOW : RED;
+          checkForWin(disc);
+        }
+      });
+
+      function getRow(disc) {
+        return discs.filter(target => {return target.y === disc.y});
+      }
+      function getCol(disc) {
+        return discs.filter(target => {return target.x === disc.x});
+      }
+      function checkForFour(arr, x) {
+        let last = 0, lastColor = '', count = 0;
+        for (let i = 0; i < arr.length; i++) {
+          const xy = x ? arr[i].x : arr[i].y;
+          if (xy !== last+1 || arr[i].color != lastColor) {
+            count = 1;
+          }
+          else {
+            count += 1;
+            if (count === 4) {
+              gameOver = true;
+            }
+          }
+          last = x ? arr[i].x : arr[i].y;
+          lastColor = arr[i].color;
+        }
+      }
+
+      function checkRowForWin(disc) {
+        let row = getRow(disc);
+        row.sort((a, b) => a.x - b.x);
+        checkForFour(row, true);
+      }
+
+      function checkColForWin(disc) {
+        let col = getCol(disc);
+        col.sort((a, b) => a.y - b.y);
+        checkForFour(col, false);
+      }
+
+      function checkForWin(disc) {
+        checkRowForWin(disc);
+        if (!gameOver) {
+          checkColForWin(disc);
+        }
+      }
+
+      function getX(x) {
+        return Math.floor((x+1) / colWidth);
+      }
+
+      function getY(x) {
+        const idx = getX(x);
+        let min = Number.MAX_VALUE;
+        for (let i = 0; i < discs.length; i++) {
+          if (discs[i].x === idx && discs[i].y < min) min = discs[i].y;
+        }
+
+        return min === Number.MAX_VALUE ? ROWS - 1 : min - 1;
+      }
+    }
+
+    function mouseMoveListener() {
+      window.addEventListener('mousemove', function(event) {
+        // if computer has the mouse and playing computer, leave
+        // if (pKbd && computer) return;
+
+        // var paddle = pKbd ? cPaddle : pPaddle;
+        // var dir = event.screenY - this.lastY;
+        // paddle.move(dir > 0 ? 10 : dir === 0 ? 0 : -10);
+        // this.lastY = event.screenY;
+      });
+    }
+
+    function addListeners() {
+      kbdListener();
+      mouseClickListener();
+      mouseMoveListener();
+    }
+
+    function clearWonLost() {
+      document.getElementById("you-won").style.display = "none";
+      document.getElementById("you-lost").style.display = "none";
+    }
+
+    ///////////////////////////////////
+    // do the work
+    ///////////////////////////////////
+    buildCanvas();
+    createGameElements();
+    addListeners();
+    clearWonLost();
+    // run step() at every animation frame, god help us otherwise
+    window.requestAnimationFrame(step) || function(step) {
+      window.setTimeout(step, 1000/60);
+    }
+
+  });
+})();
